252fa597aa9f5fb52f895fe7c3a4078ff6f4efdf
Joel Shooster <JBShooster@gmail.com>
2015-03-09 11:26:35 -0700

    Updating tests

--- shoutout/spec/features/visitors_logs_in_spec.rb
+++ shoutout/spec/features/visitors_logs_in_spec.rb
@@ -11,9 +11,17 @@ feature "Visitor logs into the app" do
     expect(page).to have_content("Here's our shouts!")
   end
 
-  scenario "and fails" do
-    
+  scenario "and password fails" do
+    visit root_path
+    fill_in "username", with: @username = "test1"
+    fill_in "password", with: @password = nil
     expect(page).to have_content("Welcome to ShoutOut!")
-  
+  end
+
+  scenario "and username fails" do
+    visit root_path
+    fill_in "username", with: @username = nil
+    fill_in "password", with: @password = "123456"
+    expect(page).to have_content("Welcome to ShoutOut!")
   end
 end
