f03fb6e894fa532d497c1a1e281b21cba52e0fb8
Joel Shooster <JBShooster@gmail.com>
2015-03-09 10:05:44 -0700

    updating tests

--- shoutout/spec/features/visitors_logs_in_spec.rb
+++ shoutout/spec/features/visitors_logs_in_spec.rb
@@ -1,13 +1,18 @@
 require "rails_helper"
 
 feature "Visitor logs into the app" do
-  scenario "success" do
+  scenario "successfully" do
     visit root_path
     #exercise
     fill_in "username", with: @username = "test1"
     fill_in "password", with: @password = "123456"
     click_button "Log in"
 
-    visit(shouts_index_path)
+    expect(page).to have_content("Here's our shouts!")
+  end
+
+  scenario "and fails" do
+    expect(page).to have_content("Welcome to ShoutOut!")
+  
   end
 end
