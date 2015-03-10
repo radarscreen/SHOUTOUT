9e9af534813ebfb2334dc0afe6a5704b84ce7e47
Joel Shooster <JBShooster@gmail.com>
2015-03-10 11:51:14 -0700

    New rspec tests created

--- shoutout/spec/features/visitor_creates_profile_spec.rb
+++ shoutout/spec/features/visitor_creates_profile_spec.rb
@@ -7,6 +7,15 @@ feature "Visitor creates a profile" do
     fill_in "profile_password", with: @password = "123456"
     fill_in "profile_email", with: @email = "testing@testerbot.com"
     click_button "Signup"
-    expect(page).to have_content("Here's our shouts!")
+    expect(page).to have_content("Here's tester123's shouts!")
+  end
+
+  scenario "and forgets a password" do
+    visit new_profile_path
+    fill_in "profile_username", with: @username = "tester123"
+    fill_in "profile_password", with: @password = nil
+    fill_in "profile_email", with: @email = "testing@testerbot.com"
+    click_button "Signup"
+    expect(page).to have_content("Create New Profile")
   end
 end
