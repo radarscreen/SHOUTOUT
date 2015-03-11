c2f13b49fa169a853799b518c255ab3354209335
Abraham Hall <abrahamhall84@gmail.com>
2015-03-11 12:07:23 -0700

    profile update

--- shoutout/app/models/profile.rb
+++ shoutout/app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ActiveRecord::Base
-  authenticates_with_sorcery!
+  #authenticates_with_sorcery!
 	#has_secure_password
 	has_many :shouts, dependent: :destroy
 	# validates :username, :email, uniqueness: true
