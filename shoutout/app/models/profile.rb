a3620cdfa4f2a4407c0b0be5209e123ca3a836a6
michael nauman <bodhiandmike@gmail.com>
2015-03-11 14:50:56 -0700

    login is working

--- shoutout/app/models/profile.rb
+++ shoutout/app/models/profile.rb
@@ -1,6 +1,5 @@
 class Profile < ActiveRecord::Base
-  #authenticates_with_sorcery!
-	#has_secure_password
+	has_secure_password
 	has_many :shouts, dependent: :destroy
 	# validates :username, :email, uniqueness: true
 	# validates :password, length: {minimum: 6}
