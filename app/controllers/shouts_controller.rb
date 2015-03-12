f9be236ceb53f9a8b19964606af89a93a0de2d2c
Paris Jones <bayarette@gmail.com>
2015-03-12 05:02:37 -0700

    added back button to shouts show page

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -14,6 +14,7 @@ class ShoutsController < ApplicationController
   def new
     @shout = Shout.new
     @categories = Category.all
+    @profile = Profile.find_by_id(params[:profile_id])
   end
 
  
