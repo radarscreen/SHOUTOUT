1402c4cceb4d78280a2fee1d2e2c95bf69ae53de
Joel Shooster <JBShooster@gmail.com>
2015-03-12 13:05:12 -0700

    Added new categories to the seed file and modified the new shouts page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,33 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Profile.create(username: 'pouchy', email: 'pouch@gmail.com', password: '123abc')
+# Profile.create(username: 'pouchy', email: 'pouch@gmail.com', password: '123abc')
 
-Profile.create(username: 'squirrely', email: 'squirrel@gmail.com', password: '123abc')
+# Profile.create(username: 'squirrely', email: 'squirrel@gmail.com', password: '123abc')
 
-Profile.create(username: 'gpom', email: 'gpom@gmail.com', password: '123abc')
+# Profile.create(username: 'gpom', email: 'gpom@gmail.com', password: '123abc')
 
-Profile.create(username: 'joel', email: 'joel@gmail.com', password: '123abc')
+# Profile.create(username: 'joel', email: 'joel@gmail.com', password: '123abc')
 
 
 
 Category.create(category: 'dining')
 
-Category.create(category: 'activity')
+Category.create(category: 'outdoors')
 
 Category.create(category: 'entertainment')
 
+Category.create(category: 'tech')
 
-Shout.create(title: 'Yoga in the Park', time: '2015/05/25 12:30:00', location: "St.Mary's Park", description: 'Come one, come all to enjoy some free yoga in the sun!', profile_id: '9', category_id: '16')
+Category.create(category: 'crafts')
 
-Shout.create(title: 'Running', time: '2015/08/16 22:17:00', location: "Embarcadero", description: 'Get your run on! Bring a friend and some shoes...', profile_id: '10', category_id: '18')
+Category.create(category: 'education')
 
-Shout.create(title: 'Iceskating', time: '2015/10/02 15:38:00', location: "Snow Lake", description: "We're practicing our pirouettes, bring your spin", profile_id: '9', category_id: '16')
 
-Shout.create(title: 'Ethiopian Food', time: '2015/09/18 07:25:00', location: "Das", description: 'Bring your appetite and a desire to get your hands dirty with some yummy food!', profile_id: '10', category_id: '17')
+# Shout.create(title: 'Yoga in the Park', time: '2015/05/25 12:30:00', location: "St.Mary's Park", description: 'Come one, come all to enjoy some free yoga in the sun!', profile_id: '9', category_id: '16')
 
-Shout.create(title: 'Chat Noir Chat Blanc', time: '2015/12/28 14:35:00', location: "2126 5th St, Berkeley", description: 'Enjoy this fab', profile_id: '11', category_id: '16')
+# Shout.create(title: 'Running', time: '2015/08/16 22:17:00', location: "Embarcadero", description: 'Get your run on! Bring a friend and some shoes...', profile_id: '10', category_id: '18')
 
-Shout.create(title: 'Kite Surfing', time: '2015/11/08 11:30:00', location: "Aquatic Park", description: 'Bruise yourself on the rocks while kitesurfing with crazies', profile_id: '12', category_id: '18')
+# Shout.create(title: 'Iceskating', time: '2015/10/02 15:38:00', location: "Snow Lake", description: "We're practicing our pirouettes, bring your spin", profile_id: '9', category_id: '16')
+
+# Shout.create(title: 'Ethiopian Food', time: '2015/09/18 07:25:00', location: "Das", description: 'Bring your appetite and a desire to get your hands dirty with some yummy food!', profile_id: '10', category_id: '17')
+
+# Shout.create(title: 'Chat Noir Chat Blanc', time: '2015/12/28 14:35:00', location: "2126 5th St, Berkeley", description: 'Enjoy this fab', profile_id: '11', category_id: '16')
+
+# Shout.create(title: 'Kite Surfing', time: '2015/11/08 11:30:00', location: "Aquatic Park", description: 'Bruise yourself on the rocks while kitesurfing with crazies', profile_id: '12', category_id: '18')
 
 
