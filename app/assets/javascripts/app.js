1cd3d98b845d06d0f8fbe326af44aa2a3980fef4
michael nauman <bodhiandmike@gmail.com>
2015-03-12 17:08:57 -0700

    updating for loop

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
 
-
   function initialize () {
     setInterval(function () {shoutLoop();}, 5000); 
   }
 
   function shoutLoop () {
-
+    console.log("test2");
     $.ajax({
-      url: "https://dataclips.heroku.com/ykzjhdqdkuzhwfrlgxmilbijctxk-profiles",
+      url: "http://localhost:3000/profiles/47",
       dataType : "json",
       success: function (data){
         data.Search.forEach(function(value) {
-          console.log(value);
+          console.log("test3");
+          console.log(value.username);
           $("body").append("<h4>" + value.username + "</h4>");
         });
+      },
+      error: function(err) {
+        console.log("test4");
       }
     });
-
-
   }
-
 
 initialize();
 
