1dfc7a96d25a8993e1bfac18dc8038519b4adf11
naumanm <bodhiandmike@gmail.com>
2015-04-08 11:00:07 -0700

    adding check to stop duplicates

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,15 +9,19 @@ $(document).ready(function() {
   function shoutLoop () {
 
     $.ajax({
-      url: "http://localhost:3000/profiles/47/shouts",
+      url: "http://localhost:3000//profiles/"+profileId+"/shouts",
       dataType : "json",
       success: function (shouts){
         shouts.forEach(function(value) {
+          
           if (value.id > currentShoutID) {
             currentShoutID = value.id;
             console.log("currentShoutID = " + currentShoutID);
-            $(".shouts").prepend("<h4>" + value.title + "</h4>");
+            console.log("currentShoutID = " + currentShoutID);
 
+            if (profileId != value.profile_id) {
+              $(".shouts").first().prepend("<li><a href=http://shoutout01.herokuapp.com/profiles/"+profileId+"/shouts/"+value.id+">"+value.title+"</a></li>");
+            }
           }
 
         });
